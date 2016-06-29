e3389bc9552243d7869f317c025f66674229af11
Mike Lewis <mlewis@beracha.org>
2016-06-29 10:55:44 -0400

    Analyzed the code, documented it a bit with my observations of what it does, and notated the things I want to change.

--- jquery.mediaTable.js
+++ jquery.mediaTable.js
@@ -35,21 +35,24 @@ http://www.consulenza-web.com/2012/01/mediatable-jquery-plugin/
  */
 
 ;(function($){
-	
-	
+
+
 	/**
-	 * DOM Initialization Logic
+	 * DOM Initialization Logic.
+	 *
+	 * This seems to basically be the constructor, though I have no idea why
+	 * it's called "loop".
 	 */
-	
 	var __loop = function( cfg, i ) {
-		
+
 		var $this 	= $(this),
 			wdg 	= $this.data( 'MediaTable' );
-		
+
 		// Prevent re-initialization of the widget!
 		if ( !$.isEmptyObject(wdg) ) return;
-		
+
 		// Build the widget context.
+		//
 		wdg = {
 			$wrap:		$('<div>'),		// Refer to the main content of the widget
 			$table:		$this,			// Refer to the MediaTable DOM (TABLE TAG)
@@ -57,238 +60,339 @@ http://www.consulenza-web.com/2012/01/mediatable-jquery-plugin/
 			cfg:		cfg,			// widget local configuration object
 			id:			$this.attr('id')
 		};
-		
+
 		// Setup Widget ID if not specified into DOM Table.
 		if ( !wdg.id ) {
 			wdg.id = 'MediaTable-' + i;
 			wdg.$table.attr( 'id', wdg.id );
 		}
-		
-		
-		
+
 		// Activate the MediaTable.
 		wdg.$table.addClass('activeMediaTable');
-		
+
 		// Create the wrapper.
 		wdg.$wrap.addClass('mediaTableWrapper');
-		
+
 		// Place the wrapper near the table and fill with MediaTable.
 		wdg.$table.before(wdg.$wrap).appendTo(wdg.$wrap);
-		
-		
+
+
 		// Menu initialization logic.
 		if ( wdg.cfg.menu ) __initMenu( wdg );
-		
+
 		// Columns Initialization Loop.
 		wdg.$table.find('thead th').each(function(i){	__thInit.call( this, i, wdg );	});
-		
-		
-		
-		
+
 		// Save widget context into table DOM.
 		wdg.$table.data( 'MediaTable', wdg );
-		
+
 	}; // EndOf: "__loop()" ###
-	
-		
+
+    /**
+     * Builds the Menu DOM object.
+     *
+     * @param object wdg
+     *   The mediatable widget object.
+     */
 		var __initMenu = function( wdg ) {
-			
+
 			// Buid menu objects
+			/*
+			 * @todo Support configurable menu DOM wrapper.
+			 *
+			 * As is, the code below creates a new DOM element (a div) into which
+			 * the menu gets inserted. Update this so that if the menuWrapper config
+			 * parameter is passed in the config JSON, the specified DOM element
+			 * will be loaded instead of creating a new DIV. NOTE: that DOM element
+			 * will need to be emptied of all contents before use.
+			 *
+			 * Config Parameter:
+			 *   - menuWrapper (string): The ID of the DOM element to use as the
+			 *     menu wrapper.
+			 *
+			 * @todo Allow the specification of custom classes to use for the
+			 * various HTML elements here?
+			 */
 			wdg.$menu 			= $('<div />');
 			wdg.$menu.$header 	= $('<a />');
 			wdg.$menu.$list		= $('<ul />');
-			
+
 			// Setup menu general properties and append to DOM.
 			wdg.$menu
 				.addClass('mediaTableMenu')
 				.addClass('mediaTableMenuClosed')
 				.append(wdg.$menu.$header)
 				.append(wdg.$menu.$list);
-			
+
 			// Add a class to the wrapper to inform about menu presence.
 			wdg.$wrap.addClass('mediaTableWrapperWithMenu');
-			
+
 			// Setup menu title (handler)
+			/*
+			 * @todo Support configuration of Menu Icon/Image instead of Text.
+			 */
 			wdg.$menu.$header.text(wdg.cfg.menuTitle);
 			wdg.$table.before(wdg.$menu);
-			
+
 			// Bind screen change events to update checkbox status of displayed fields.
 			$(window).bind('orientationchange resize',function(){
 				wdg.$menu.find('input').trigger('updateCheck');
 			});
-			
+
 			// Toggle list visibility when clicking the menu title.
 			wdg.$menu.$header.bind('click',function(){
 				wdg.$menu.toggleClass('mediaTableMenuClosed');
 			});
-			
+
 			wdg.$table.click(function() {
                 wdg.$menu.addClass('mediaTableMenuClosed');
             });
-			
+
 			// Toggle list visibilty when mouse go outside the list itself.
 			wdg.$menu.$list.bind('mouseleave',function(e){
 				wdg.$menu.toggleClass('mediaTableMenuClosed');
 				e.stopPropagation();
 			});
-			
-			
-			
+
+
+
 		}; // EndOf: "__initMenu()" ###
-		
+
 		var __thInit = function( i, wdg ) {
-			
+
 			var $th 	= $(this),
 				id		= $th.attr('id'),
 				classes = $th.attr('class');
-			
+				// @todo Check to see if this column should be hidden by default and
+				// set the appropriate attributes.
+
 			// Set up an auto-generated ID for the column.
 			// the ID is based upon widget's ID to allow multiple tables into one page.
 			if ( !id ) {
 				id = wdg.id + '-mediaTableCol-' + i;
 				$th.attr( 'id', id );
 			}
-			
+
 			// Add toggle link to the menu.
 			if ( wdg.cfg.menu && !$th.is('.persist') ) {
-				
+				// @todo Hide by default if configged?
+				// This may be where the logic to hide by default should be added.
+
+				/*
+				 * @todo Enable better configuration of the rendering of menu items.
+				 *
+				 * This is where we can provide additional control over the rendering
+				 * of elements. I thought maybe of exposing the following configurable
+				 * options through wdg.cfg:
+				 *
+				 * 1) useCheckbox (bool): true/false to insert the checkbox or not.
+				 * 2) selectedClass (string): the class name to use for menu items
+				 *    that are visible.
+				 * 3) hiddenClass (string): the class name to use for menu items that
+				 *    are hidden.
+				 *
+			   * This would allow the theme layer to have greater control over the
+			   * styling of the row - to do things such as highlighting selected
+			   * rows one color and hidden rows another, for instance.
+			   *
+			   * After reviewing the code below (in __liInitActions()), supporting
+			   * non-checkbox items would require some overhaul that I don't want to
+			   * get into if I don't have to. It seems simplest to just hide the
+			   * checkbox with CSS, so if the designers don't want the checkbox to
+			   * show, they should simply hide it that way.
+			   *
+			   * That would only allow hiding or showing of ALL checkboxes. There's
+			   * not currently support for hiding checkboxes of hidden columns while
+			   * showing checkboxes of visible columns or something like that. I
+			   * can't imagine that would be desired, but who knows.
+			   */
 				var $li = $('<li><input type="checkbox" name="toggle-cols" id="toggle-col-'+wdg.id+'-'+i+'" value="'+id+'" /> <label for="toggle-col-'+wdg.id+'-'+i+'">'+$th.text()+'</label></li>');
 				wdg.$menu.$list.append($li);
-				
+
 				__liInitActions( $th, $li.find('input'), wdg );
-				
+
 			}
-			
+
 			// Propagate column's properties to each cell.
-			$('tbody tr',wdg.$table).each(function(){	__trInit.call( this, i, id, classes ); });
-			
+			$('tbody tr',wdg.$table).each(function(){	__trInit.call( this, i, id, classes, $th ); });
+
 		}; // EndOf: "__thInit()" ###
-		
-		
-		var __trInit = function( i, id, classes ) {
-			
+
+
+		/**
+		 * Initializes mediatable functionality for each row of a column.
+		 *
+		 * @param int i
+		 *   The column index of the column.
+		 * @param string id
+		 *   The HTML ID of the header cell.
+		 * @param string classes
+		 *   The HTML classes of the header cell.
+		 * @param object th
+		 *   The header cell DOM object.
+		 */
+		var __trInit = function( i, id, classes, th ) {
+
 			var $cell	= $(this).find('td,th').eq(i);
-			
+
 			$cell.attr( 'headers', id );
-			
+
 			if ( classes ) $cell.addClass(classes);
-			
+
+			// @todo Check to see if this column should be hidden by default and
+			// set the appropriate attributes. Do I need to pass additional
+			// arguments? I might need to pass the row here....
+
 		}; // EndOf: "__trInit()" ###
-		
-		
+
+		/**
+		 * Adds event handlers to a column's menu item in the drop down.
+		 *
+		 * @param object $th
+		 *   The Column header cell.
+		 * @param object $checkbox
+		 *   The Menu item checkbox DOM object.
+		 * @param object wdg
+		 *   The MediaTable widget object.
+		 */
 		var __liInitActions = function( $th, $checkbox, wdg ) {
-			
+			/*
+			 * @todo Swap the hidden/visible classes when an item is changed.
+			 * Not sure if this needs to happen in change() or updateChecked, but it
+			 * needs to happen somewhere in here.
+			 */
+
+			/**
+			 * Hide or show a column.
+			 *
+			 * This is the callback function that will be executed when a column
+			 * name is clicked in the menu to be either hidden or shown.
+			 */
 			var change = function() {
-				
+				/*
+				 * @todo Support Drupal Sticky Headers here?
+				 * This may or may not be the best place to support sticky headers...
+				 *
+				 * It seems like bad design to do it here. That would be inserting a
+				 * dependency or at least a specification upstream of where it needs
+				 * to be. If I put Drupal Specific code here, then that Drupal
+				 * specific code would be present even if this was used outside the
+				 * Drupal context. That doesn't make sense. Perhaps I should look into
+				 * adding a secondary listener to these behaviors in my code. I will
+				 * have to figure out how to do that...
+				 */
 				var	val 	= $checkbox.val(),  // this equals the header's ID, i.e. "company"
 					cols 	= wdg.$table.find("#" + val + ", [headers="+ val +"]"); // so we can easily find the matching header (id="company") and cells (headers="company")
-				
-				
-				if ( $checkbox.is(":checked")) { 
+
+
+				if ( $checkbox.is(":checked")) {
 					cols.show();
-					 
-				} else { 
+
+				} else {
 					cols.hide();
-					
+
 				};
-				
+
 			};
-			
+
+			/**
+			 * Update the checkbox of a menu item.
+			 *
+			 * This callback function updates the checkbox of a menu item dependent
+			 * upon whether it is visible or not.
+			 */
 			var updateCheck = function() {
-				
-				//if ( $th.css("display") ==  "table-cell") {
+				/*
+				 * @todo Support hidden by default?
+				 *
+				 * Maybe I need to do something here to reflect the hidden by
+				 * default option? We'll see. If what I do upstream of this results in
+				 * the element not being :visible, then I don't think I need to do
+				 * anything.
+				 */
 				if ( $th.is(':visible') ) {
 					$checkbox.attr("checked", true);
 				}
 				else {
 					$checkbox.attr("checked", false);
 				};
-			
+
 			};
-			
+
+			// ML (2016/06/29): I'm not entirely certain how this chaining works...
+			// I don't think I need to mess with it, though, so unless it's really
+			// not working the way you want it to, leave it alone!
 			$checkbox
 				.bind('change',	change )
 				.bind('updateCheck', updateCheck )
 				.trigger( 'updateCheck' );
-		
+
 		} // EndOf: "__liInitActions()" ###
-	
-	
-	
-	
-	
-	
-	
+
 	/**
-	 * Widget Destroy Logic
+	 * Destructor function for the widget.
 	 */
-	
 	var __destroy = function() {
-		
+
 		// Get the widget context.
 		var wdg = $(this).data( 'MediaTable' );
 		if ( !wdg ) return;
-		
-		
+
+
 		// Remove the wrapper from the MediaTable.
 		wdg.$wrap.after(wdg.$table).remove();
-		
+
 		// Remove MediaTable active class so media-query will not work.
 		wdg.$table.removeClass('activeMediaTable');
-		
-		
+
+
 		// Remove DOM reference to the widget context.
 		wdg.$table.data( 'MediaTable', null );
-		
+
 	}; // EndOf: "__destroy()" ###
-	
-	
-	
-	
-	
-	
-	
+
+
 	/**
 	 * jQuery Extension
 	 */
 	$.fn.mediaTable = function() {
-		
+
 		var cfg = false;
-		
+
 		// Default configuration block
 		if ( !arguments.length || $.isPlainObject(arguments[0]) ) cfg = $.extend({},{
-			
+
 			// Teach the widget to create a toggle menu to declare column's visibility
 			menu:		true,
 			menuTitle:	'Columns:',
-			
+
 		t:'e'},arguments[0]);
 		// -- default configuration block --
-		
-		
-		
-		// Items initialization loop:	
+
+
+
+		// Items initialization loop:
 		if ( cfg !== false ) {
 			$(this).each(function( i ){ __loop.call( this, cfg, i ); });
-			
-			
-			
- 			
+
+
+
+
 		// Item actions loop - switch throught actions
 		} else if ( arguments.length ) switch ( arguments[0] ) {
-		
+
 			case 'destroy':
 			$(this).each(function(){ __destroy.call( this ); });
 			break;
-		
+
 		}
-		
-		
-		// Mantengo la possibilitˆ di concatenare plugins.
+
+
+		// Mantengo la possibilit? di concatenare plugins.
 		return this;
-		
+
 	}; // EndOf: "$.fn.mediaTable()" ###
-	
-	
+
+
 })( jQuery );
 
